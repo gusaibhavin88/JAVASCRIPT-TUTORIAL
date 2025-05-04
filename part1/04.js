0b3bfb94ab52725ed9efff80f64f2633cc5009bd
Bhavin <gusaibhavin88@gmail.com>
2025-05-04 16:53:16 +0530

    changes up

--- part1/04.js
+++ part1/04.js
@@ -1,9 +1,21 @@
-// let keyword 
-// declare variable with let keyword 
+// let keyword
+// declare variable with let keyword
 
 let firstName = "harshit";
 firstName = "Mohit";
 console.log(firstName);
 
-
 // block scope vs funtion scope (covered later in this video)
+
+function test() {
+  if (true) {
+    var x = 10;
+  }
+  if (true) {
+    let y = 20;
+    const z = 30;
+  }
+  console.log(y); // ❌ ReferenceError
+  console.log(z); // ❌ ReferenceError
+}
+test();
--- part1/49.js
+++ part1/49.js
@@ -1,20 +1,19 @@
-// lexical scope 
+// lexical scope
+// Functions remember the variables from the environment where they were defined, not where they are called.
+
 const myVar = "value1";
 
-function myApp(){
-    
+function myApp() {
+  function myFunc() {
+    // const myVar = "value59";
+    const myFunc2 = () => {
+      console.log("inside myFunc", myVar);
+    };
+    myFunc2();
+  }
 
-    function myFunc(){
-        // const myVar = "value59";
-        const myFunc2 = () =>{
-            console.log("inside myFunc", myVar);
-        }
-        myFunc2();
-    }
-
-
-    console.log(myVar);
-    myFunc();
+  console.log(myVar);
+  myFunc();
 }
 
 myApp();
